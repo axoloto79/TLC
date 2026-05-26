5fa235c9455acc3a9cc77aeb92216802ddb3210e
TagCopperLight <tagcopperlight@gmail.com>
2026-05-26 15:48:13 +0200

    ip en moins dans la vm

--- src/vm.py
+++ src/vm.py
@@ -2,7 +2,6 @@ import ast
 
 class VirtualMachine:
     def __init__(self):
-        self.ip = -1  # Pointeur de la pile (-1 car pointe vers avant la première case car la pile est vide)
         self.stack = [] # Pile d'exécution
         self.base = -2  # Base du bloc de liaison (-2 car la pile est vide)
         self.pc = 0 # Compteur ordinal
@@ -78,7 +77,6 @@ class VirtualMachine:
             arg = instr[1] # None si pas d'argument
 
             if op == "debutProg":
-                self.ip = -1 # initialise le pointeur de la pile
                 self.base = -2 # initialise la base du bloc de liaison
 
             elif op == "finProg":
@@ -89,17 +87,14 @@ class VirtualMachine:
             elif op == "reserver":
                 for _ in range(arg):
                     self.stack.append(None) # alloue une case dans la pile pour la variable
-                self.ip += arg # incrémente le pointeur de la pile
 
             elif op == "empiler":
                 self.stack.append(arg)
-                self.ip += 1 # incrémente le pointeur de la pile
 
             elif op == "affectation":
                 value = self.stack.pop() # dépile la valeur à affecter
                 address = self.stack.pop() # dépile l'adresse de la variable à affecter
                 self.stack[address] = value # affecte la valeur à la variable
-                self.ip -= 2 # décrémente le pointeur de la pile
 
             elif op == "valeurPile":
                 address = self.stack.pop() # récupère l'adresse de la variable
@@ -109,33 +104,28 @@ class VirtualMachine:
             elif op == "get":
                 address = self.stack.pop() # récupère l'adresse de la variable à affecter
                 self.stack[address] = int(input("Entrez une valeur entière: "))
-                self.ip -= 1 # décrémente le pointeur de la pile
 
             elif op == "put":
                 value = self.stack.pop() # dépile la valeur à afficher
                 print(value) # affiche la valeur
-                self.ip -= 1 # décrémente le pointeur de la pile
 
             elif op == "moins":
-                self.stack[self.ip] = -self.stack[self.ip] # inverse le signe de la valeur
+                self.stack[-1] = -self.stack[-1] # inverse le signe de la valeur
 
             elif op == "sous":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(a - b)
-                self.ip -= 1
 
             elif op == "add":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(a + b)
-                self.ip -= 1
 
             elif op == "mult":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(a * b)
-                self.ip -= 1
 
             elif op == "div":
                 b = self.stack.pop()
@@ -143,55 +133,46 @@ class VirtualMachine:
                 if b == 0:
                     raise Exception("Division par zéro") # erreur si division par zéro
                 self.stack.append(a // b)
-                self.ip -= 1
 
             elif op == "egal":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(int(a == b)) # empile 1 si a == b, sinon 0 ie empile le code de a==b
-                self.ip -= 1
 
             elif op == "diff":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(int(a != b)) # empile 1 si a != b, sinon 0 ie empile le code de a!=b
-                self.ip -= 1
 
             elif op == "inf":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(int(a < b)) #empile 1 si a < b, sinon 0 ie empile le code de a<b
-                self.ip -= 1
 
             elif op == "infeg":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(int(a <= b)) # empile 1 si a <= b, sinon 0 ie empile le code de a<=b
-                self.ip -= 1
 
             elif op == "sup":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(int(a > b)) # empile 1 si a > b, sinon 0 ie empile le code de a>b
-                self.ip -= 1
 
             elif op == "supeg":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(int(a >= b)) # empile 1 si a >= b, sinon 0 ie empile le code de a>=b
-                self.ip -= 1
 
             elif op == "et":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(int(a and b)) # empile 1 si a et b, sinon 0 ie empile le code de a and b
-                self.ip -= 1
 
             elif op == "ou":
                 b = self.stack.pop()
                 a = self.stack.pop()
                 self.stack.append(int(a or b)) # empile 1 si a ou b, sinon 0 ie empile le code de a or b
-                self.ip -= 1
 
             elif op == "non":
                 a = self.stack.pop()
@@ -202,7 +183,6 @@ class VirtualMachine:
 
             elif op == "tze": # branchement si le sommet de la pile est nul
                 code = self.stack.pop()
-                self.ip -= 1
                 if code == 0:
                     self.pc = arg
 
@@ -217,24 +197,21 @@ class VirtualMachine:
             elif op == "empilerAd":
                 address_dyn = self.base + 2 + arg # adresse dynamique (+2 pour passer le bloc de liaison est arrivé dans le bloc des paramètre/variables locales)
                 self.stack.append(address_dyn) # empile l'adresse dynamique de la variable
-                self.ip += 1 # incrémente le pointeur de la pile
 
             elif op == "reserverBloc":
                 self.stack.append(self.base) # empile la base du bloc de liaison précedent
                 self.stack.append(None) # reserve une case pour l'adresse de retour
-                self.ip += 2 # incremente le pointeur de la pile
 
             elif op == "traStat":
                 a = arg[0]
                 nbp = arg[1] # nbp = nombre de paramètres
-                self.base = self.ip - nbp -1 # mets à jour la base du bloc de liaison
-                self.stack[self.base + 1] = self.pc # mets à jour l'adresse de retour (self.pc-1 ?)
+                self.base = len(self.stack) - nbp - 2 # mets à jour la base du bloc de liaison
+                self.stack[self.base + 1] = self.pc # mets à jour l'adresse de retour
                 self.pc = a # mets à jour le pc pour aller à l'instruction suivante de la fonction/procédure
 
             elif op == "retourFonct":
                 valeur = self.stack.pop() # récupère la valeur de retour
                 old_base = self.base # récupère l'ancienne valeur de la base
-                self.ip = old_base # mets à jour le pointeur de la pile
                 self.base = self.stack[old_base] # récupère la base du bloc de liaison précedent et mets à jour la base
                 self.pc = self.stack[old_base + 1] # récupère l'adresse de retour et mets à jour le pc
                 del self.stack[old_base:] # supprime les cases utilisées par la fonction
@@ -243,7 +220,6 @@ class VirtualMachine:
             elif op == "retourProc":
                 old_base = self.base # récupère l'ancienne valeur de la base
                 self.base = self.stack[old_base] # récupère la base du bloc de liaison précedent et mets à jour la base
-                self.ip = old_base - 1 # mets à jour le pointeur de la pile
                 self.pc = self.stack[old_base + 1] # récupère l'adresse de retour et mets à jour le pc
                 del self.stack[old_base:] # supprime les case utilisée par la procédure
 
@@ -251,7 +227,6 @@ class VirtualMachine:
                 address = arg # adresse du paramètre
                 address_dyn = self.base + 2 + address # calcul de l'adresse dynamique du paramètre
                 self.stack.append(self.stack[address_dyn]) # empile la valeur de l'adresse du paramètre
-                self.ip += 1 # incrémente le pointeur de la pile
 
             else:
                 raise Exception(f"Instruction inconnue : {op}, {arg}") # erreur si tout autre instruction
